27df9fe8712fed1dc259136ebd2f7f644c0593cd
Gerardo Rodriguez <jerryprofilo32@gmail.com>
2024-02-28 19:34:40 -0500

    update user profile

--- components/userDetail/userDetail.jsx
+++ components/userDetail/userDetail.jsx
@@ -13,32 +13,30 @@ class UserDetail extends React.Component {
         console.log(user);
 
         return (
-            <Paper elevation={3} style={{ padding: '20px', marginTop: '20px' }}>
+            <Paper elevation={3} className="user-detail-container">
                 <Typography variant="h4" component="h1" gutterBottom>
                     User Profile
                 </Typography>
                 {user ? (
-                    <Box display="flex" flexDirection="column" alignItems="center">
-                        <img
-                            src={user.image }
-                            alt={user.name}
-                            style={{ width: '150px', height: '150px', borderRadius: '50%', marginBottom: '20px' }}
-                        />
-                        <Typography variant="h5" component="h2">
-                            {user.name}
-                        </Typography>
-                        <Typography variant="body1">
-                            Email: {user.email}
-                        </Typography>
-                        <Typography variant="body1">
-                            Location: {user.location}
-                        </Typography>
-                        <Typography variant="body1">
-                            Occupation: {user.occupation }
-                        </Typography>
-                        <Typography variant="body1">
-                            Description: {user.description }
-                        </Typography>
+                    <Box className="user-content">
+                        <Box className="user-photo-box">
+                            <img
+                                src={user.image}
+                                alt={user.name}
+                                className="user-photo"
+                            />
+                        </Box>
+                        <Box className="user-info-box">
+                            <Typography variant="h5" component="h2">
+                                {user.name}
+                            </Typography>
+                            <Typography variant="body1">
+                                Location: {user.location}
+                            </Typography>
+                            <Typography variant="body1">
+                                Occupation: {user.occupation}
+                            </Typography>
+                        </Box>
                     </Box>
                 ) : (
                     <Typography variant="body1">
@@ -47,6 +45,7 @@ class UserDetail extends React.Component {
                 )}
             </Paper>
         );
+
     }
 }
 
