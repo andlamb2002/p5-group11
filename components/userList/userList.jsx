b1342b908650beee2fdfa8c1345b7adc527b7063
Andreas Lambropoulos <andlamb2002@gmail.com>
2024-03-14 11:48:47 -0400

    Implemented axios in user list

--- components/userList/userList.jsx
+++ components/userList/userList.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
-//import { Divider, List, ListItemButton, ListItemText,}from '@mui/material';
 import { Divider, List, ListItem, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 import './userList.css';
-import { fetchModel } from '../../lib/fetchModelData';
+// import { fetchModel } from '../../lib/fetchModelData';
+import axios from 'axios';
 
 class UserList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
                 userList: [],
-                /*users: undefined,
-                user_id: undefined*/ 
             };
     }
 
     componentDidMount() {
         const url = '/user/list';
-
-        fetchModel(url).then((response) => {
-            this.setState({ userList: response.data });
-        })
-        .catch((error) => {
-            console.error('Error fetching user List:', error);
-        });
+    
+        axios.get(url)
+            .then((response) => {
+                this.setState({ userList: response.data });
+            })
+            .catch((error) => {
+                console.error('Error fetching user List:', error);
+            });
     }
 
     handleUserClick = (user) => {
@@ -63,54 +62,6 @@ class UserList extends React.Component {
         );
     }
 
-/*
-    componentDidUpdate() {
-        const new_user_id = this.props.match?.params.userId;
-        const current_user_id = this.state.user_id;
-        if (current_user_id  !== new_user_id){
-            this.handleUserChange(new_user_id);
-        }
-    }
-
-    handleUserChange(user_id){
-        this.setState({
-            user_id: user_id
-        });
-    }
-
-    handleUserListChange() {
-        const url = 'http://localhost:3000/user/list'; 
-        fetchModel(url)
-            .then(response => {
-                this.setState({
-                    users: response.data 
-                });
-            })
-            .catch(error => {
-                console.error('Error fetching user list:', error);
-            });
-    }
-
-    render() {
-      return this.state.users ?(
-          <div>
-          <List component="nav">
-              {
-                  this.state.users.map(user => (
-                  <ListItemButton selected={this.state.user_id === user._id}
-                                  key={user._id}
-                                  divider={true}
-                                  component="a" href={"#/users/" + user._id}>
-                      <ListItemText primary={user.first_name + " " + user.last_name} />
-                  </ListItemButton>
-              ))
-              }
-          </List>
-          </div>
-      ) : (
-          <div/>
-      );
-    } */
 }
 
 export default UserList;
