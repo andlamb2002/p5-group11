2545e65d3da4b2904763cea1bd3f8dd0d7779431
Andreas Lambropoulos <andlamb2002@gmail.com>
2024-03-20 12:03:30 -0400

    Updated user schema with username password

--- schema/user.js
+++ schema/user.js
@@ -6,11 +6,13 @@ const mongoose = require("mongoose");
  * Define the Mongoose Schema for a Comment.
  */
 const userSchema = new mongoose.Schema({
-  first_name: String,
-  last_name: String,
-  location: String,
-  description: String,
-  occupation: String,
+  first_name: { type: String, default: "Jane" }, 
+  last_name: { type: String, default: "Doe" },
+  location: { type: String, default: "New York, NY" },
+  description: { type: String, default: "Photography enthusiast" },
+  occupation: { type: String, default: "Software Engineer" },
+  login_name: { type: String, required: true }, 
+  password: { type: String, required: true } 
 });
 
 /**
