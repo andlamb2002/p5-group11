622562dcbf6b2d7da34da13817490b6c1330f909
Sayya Teja Swaroop <63931564+tejasayya@users.noreply.github.com>
2024-03-29 16:49:08 -0400

    Update userList.jsx

--- components/userList/userList.jsx
+++ components/userList/userList.jsx
@@ -1,66 +1,75 @@
 import React from 'react';
-import { Divider, List, ListItem, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
+import {
+  List,
+  ListItemButton,
+  ListItemText,
+}
+from '@mui/material';
 import './userList.css';
 import axios from 'axios';
 
+/**
+ * Define UserList, a React component of project #5
+ */
 class UserList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-                userList: [],
+                users: undefined,
+                user_id: undefined
             };
     }
 
     componentDidMount() {
-        const url = '/user/list';
-    
-        axios.get(url)
-            .then((response) => {
-                this.setState({ userList: response.data });
-            })
-            .catch((error) => {
-                console.error('Error fetching user List:', error);
+        this.handleUserListChange();
+    }
+
+    componentDidUpdate() {
+        const new_user_id = this.props.match?.params.userId;
+        //console.log(new_user_id);
+        const current_user_id = this.state.user_id;
+        //console.log(current_user_id);
+        if (current_user_id  !== new_user_id){
+            this.handleUserChange(new_user_id);
+        }
+    }
+
+    handleUserChange(user_id){
+        this.setState({
+            user_id: user_id
+        });
+    }
+
+    handleUserListChange(){
+        axios.get("/user/list")
+            .then((response) =>
+            {
+                this.setState({
+                    users: response.data
+                });
             });
     }
 
-    handleUserClick = (user) => {
-        this.props.setTopName(`User Details of ${user.first_name} ${user.last_name}`);
-    };
-
-    render() {
-        const { userList } = this.state;
-        
-        return (
-         <div >
-           <div>
-             <List component="nav">
-               {userList.map((user) => (
-                 <div key={user._id}>
-                   <ListItem>
-                     <Button
-                     onClick={() => {
-                       this.handleUserClick(user); 
-                   }}
-                       component={Link}
-                       to={`/users/${user._id}`}
-                       className="ButtonStyle" 
-                     >
-                       {`${user.first_name} ${user.last_name}`}
-                     </Button>
-                   </ListItem>
-                   <Divider />
-                 </div>
-               ))}
-             </List>
-           </div>
-           <div style={{ width: '70%' }}>
-             {this.props.children}
-           </div>
-         </div>
-        );
-    }
-
+  render() {
+    return this.state.users ?(
+        <div>
+        <List component="nav">
+            {
+                this.state.users.map(user => (
+                <ListItemButton selected={this.state.user_id === user._id}
+                                key={user._id}
+                                divider={true}
+                                component="a" href={"#/users/" + user._id}>
+                    <ListItemText primary={user.first_name + " " + user.last_name} />
+                </ListItemButton>
+            ))
+            }
+        </List>
+        </div>
+    ) : (
+        <div/>
+    );
+  }
 }
 
 export default UserList;
